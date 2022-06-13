dab1e7caefe242b7f62dd727c362638d661917b1
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-06-13 21:38:17 +0500

    updated

--- Modules/Main/Http/Actions/Home/IndexHome.php
+++ Modules/Main/Http/Actions/Home/IndexHome.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Main\Http\Actions\Home;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Sarfraznawaz2005\Actions\Action;
 
 class IndexHome extends Action
@@ -10,6 +12,79 @@ class IndexHome extends Action
     {
         title('Dashboard');
 
-        return view('main::pages.home.index');
+        $amountDueToday = DB::table('entries')
+            ->select('amount')
+            ->whereDate('created_at', Carbon::today())
+            ->where('paid', '0')
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('amount');
+
+        $bottlesDueToday = DB::table('entries')
+            ->select(DB::raw('(bottles_sent - bottles_received) as cnt'))
+            ->whereDate('created_at', Carbon::today())
+            ->where('bottles_sent', '>', 'bottles_received')
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('cnt');
+
+        $amountDueTotal = DB::table('entries')
+            ->select('amount')
+            ->whereDate('created_at', Carbon::today())
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('amount');
+
+        $amountDueMonth = DB::table('entries')
+            ->select('amount')
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->where('paid', '0')
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('amount');
+
+        $bottlesDueMonth = DB::table('entries')
+            ->select(DB::raw('(bottles_sent - bottles_received) as cnt'))
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->where('bottles_sent', '>', 'bottles_received')
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('cnt');
+
+        $amountDueTotalMonth = DB::table('entries')
+            ->select('amount')
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('amount');
+
+        $amountDueOverall = DB::table('entries')
+            ->select('amount')
+            ->where('paid', '0')
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('amount');
+
+        $bottlesDueOverall = DB::table('entries')
+            ->select(DB::raw('(bottles_sent - bottles_received) as cnt'))
+            ->where('bottles_sent', '>', 'bottles_received')
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('cnt');
+
+        $amountDueTotalOverall = DB::table('entries')
+            ->select('amount')
+            ->where('is_monthly', '0')
+            ->get()
+            ->sum('amount');
+
+        return view(
+            'main::pages.home.index',
+            compact(
+                'amountDueToday', 'bottlesDueToday', 'amountDueTotal',
+                'amountDueMonth', 'bottlesDueMonth', 'amountDueTotalMonth',
+                'amountDueOverall', 'bottlesDueOverall', 'amountDueTotalOverall'
+            )
+        );
     }
 }
--- Modules/Main/Resources/views/pages/home/index.blade.php
+++ Modules/Main/Resources/views/pages/home/index.blade.php
@@ -2,18 +2,132 @@
 
 @section('content')
 
-    <div class="jumbotron text-center">
-        <h1>{{config('app.name')}}</h1>
-        <p class="padded">
-            Welcome to Laravel Starter!
-        </p>
-        <p>
-            @guest
-                <a class="btn btn-lg btn-success btn-raised" href="{{ url('/user/register') }}" role="button">
-                    <i class="fa fa-paper-plane"></i> Get Started!
-                </a>
-            @endguest
-        </p>
+    <span class="badge badge-warning" style="font-size: 16px;">Today</span>
+    <hr style="margin-top: 5px;">
+
+    <div class="row">
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small primary"><i class="icon fa fa-money fa-3x"></i>
+                    <div class="info">
+                        <h4>Amount Due</h4>
+                        <h3>{{$amountDueToday}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small danger"><i class="icon fa fa-flask fa-3x"></i>
+                    <div class="info">
+                        <h4>Bottles Due</h4>
+                        <h3>{{$bottlesDueToday}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small success"><i class="icon fa fa-money fa-3x"></i>
+                    <div class="info">
+                        <h4>Total Amount (Paid/Unpaid)</h4>
+                        <h3>{{$amountDueTotal}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+    <span class="badge badge-warning" style="font-size: 16px;">This Month</span>
+    <hr style="margin-top: 5px;">
+
+    <div class="row">
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small primary"><i class="icon fa fa-money fa-3x"></i>
+                    <div class="info">
+                        <h4>Amount Due</h4>
+                        <h3>{{$amountDueMonth}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small danger"><i class="icon fa fa-flask fa-3x"></i>
+                    <div class="info">
+                        <h4>Bottles Due</h4>
+                        <h3>{{$bottlesDueMonth}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small success"><i class="icon fa fa-money fa-3x"></i>
+                    <div class="info">
+                        <h4>Total Amount (Paid/Unpaid)</h4>
+                        <h3>{{$amountDueTotalMonth}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+    <span class="badge badge-warning" style="font-size: 16px;">Overall</span>
+    <hr style="margin-top: 5px;">
+
+    <div class="row">
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small primary"><i class="icon fa fa-money fa-3x"></i>
+                    <div class="info">
+                        <h4>Amount Due</h4>
+                        <h3>{{$amountDueOverall}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small danger"><i class="icon fa fa-flask fa-3x"></i>
+                    <div class="info">
+                        <h4>Bottles Due</h4>
+                        <h3>{{$bottlesDueOverall}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-md-4">
+            <a href="#" class="widget-link">
+                <div class="widget-small success"><i class="icon fa fa-money fa-3x"></i>
+                    <div class="info">
+                        <h4>Total Amount (Paid/Unpaid)</h4>
+                        <h3>{{$amountDueTotalOverall}}</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
+
     </div>
 
 @stop
+
+@push('styles')
+    <style>
+        .widget-small .info h4 {
+            font-weight: bold;
+            font-size: 1rem;
+        }
+    </style>
+@endpush
